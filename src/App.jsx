aaf2028da4689eb9b8b6baf7cd3f4480af049fc9
sebastianhoyoslopez <sebastianhoyos145@gmail.com>
2021-06-02 09:51:05 -0500

    updated palindromo

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function App() {
+  const [word, setWord] = useState("");
+  const palindromo = (cadena) => {
+    const palabra = cadena.split("");
+    const reverse = palabra.reverse();
+    setWord("")
+    return cadena === reverse.join("")
+      ? console.log("si es")
+      : console.log("no es");
+  };
+
+  
+
+  return (
+    <div className="container my-5">
+      <h1>Comprobar palindromo</h1>
+      <input value={word} onChange={(e) => setWord(e.target.value)} />
+      <button className="mt-5" onClick={() => palindromo(word)}>
+        probar
+      </button>
+      <div>
+        <p></p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
