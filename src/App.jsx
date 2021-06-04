e7b522304681113f64fa336200097960c7bfd8b7
sebastianhoyoslopez <sebastianhoyos145@gmail.com>
2021-06-03 22:24:20 -0500

    the palindrome was updated in the HTML

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,41 @@ import React, { useState } from "react";
 
 function App() {
   const [word, setWord] = useState("");
-  const palindromo = (cadena) => {
-    const palabra = cadena.split("");
+  const [words, setWords] = useState([]);
+
+  const palindromo = () => {
+    const palabra = word.split("");
     const reverse = palabra.reverse();
     setWord("");
-    return cadena === reverse.join("")
-      ? console.log("si es")
-      : console.log("no es");
+    setWords([
+      ...words,
+      { name: word, esPalindromo: word === reverse.join("") ? true : false },
+    ]);
   };
 
   return (
     <div className="container my-5">
       <h1>Comprobar palindromo</h1>
       <input value={word} onChange={(e) => setWord(e.target.value)} />
-      <button className="mt-5" onClick={() => palindromo(word)}>
+      <button className="mt-5" onClick={() => palindromo()}>
         probar
       </button>
       <div>
-        <p></p>
+        <div>
+          {words.map((wordtext, index) => (
+            <div key={index}>
+              <h3>{wordtext.name}</h3>
+              <div>
+                Resultado:
+                {wordtext.esPalindromo && wordtext.name ? (
+                  <div>Si es un palindromo</div>
+                ) : (
+                  <div>No un palindromo</div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
